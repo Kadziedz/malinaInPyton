0b5cf85d52c1ba281f81e57b64d8d660f90bf707
Krzysztof Dziedzicki <kadziedz@gmail.com>
2023-08-06 21:41:58 +0200

    web

---
+++ html/config.php
@@ -0,0 +1,8 @@
+<?php
+define("SQL_HOST",  "localhost");
+define("SQL_USER", "kade");
+define("SQL_PASS", "wp00scmnie!");
+define("SQL_DB",  "ControlDB");
+define("WEBAPI_URL",  "http://localhost:5000/");
+define("ADODB_PATH", 'adodb5');
+?>
